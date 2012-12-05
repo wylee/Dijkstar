15027ba7b86061eba9a415e9c767bebb3b551c2a
Wyatt <self@wyattbaldwin.com>
2012-12-04 18:53:56 -0800

    Restructure package
    - Move path finding functions to new module, algorithm
    - Add imports of find_path() and Graph in __init__ to establish public
      API

--- dijkstar/algorithm.py
+++ dijkstar/algorithm.py
@@ -1,22 +1,3 @@
-###############################################################################
-# Created 2004-12-28.
-#
-# Dijkstra/A* path-finding functions.
-#
-# Copyright (C) 2004-2007, 2012 Wyatt Baldwin. All rights reserved.
-#
-# Licensed under the MIT license.
-#
-#    http://www.opensource.org/licenses/mit-license.php
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-###############################################################################
 """Dijkstra/A* path-finding functions."""
 import heapq
 
