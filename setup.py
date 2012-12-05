4aa958524299a6f467c350a7ce1131e0dc849044
Wyatt <self@wyattbaldwin.com>
2012-12-04 21:24:04 -0800

    Start preparing for 2.0 release
    - Move long description out of setup() and into new README doc
    - Flesh out README a bit; add examples
    - Update setup()
      - Set version to 2.0.dev0 since next release will backward
        incompatible
      - Change license to just MIT (remove BSD)
      - Note the specific versions of Python supported (2.6 and 2.7)
    
        That's got me wondering what it will take to get this running on
        Python 3--maybe nothing or not much since there are no dependencies
        other than stdlib.

--- setup.py
+++ setup.py
@@ -1,46 +1,26 @@
 from distutils.core import setup
 
 
+with open('README') as fp:
+    long_description = fp.read()
+
+
 setup(
     name='Dijkstar',
-    version='1.0',
-    description='Dijkstra/A* path finding functions',
-    long_description="""
-Dijkstar
-++++++++
-
-Dijkstar is an implementation of Dijkstra's single-source shortest-paths
-algorithm. If a destination node is given, the algorithm halts when that
-node is reached; otherwise it continues until paths from the source node
-to all other nodes are found.
-
-Accepts an optional cost (or "weight") function that will be called on
-every iteration.
-
-Also accepts an optional heuristic function that is used to push the
-algorithm toward a destination instead of fanning out in every
-direction. Using such a heuristic function converts Dijkstra to A* (and
-this is where the name "Dijkstar" comes from).
-
-Performance is decent on a graph with 100,000+ nodes. Runs in around .5
-seconds on average .
-
-See the source for the required graph structure:
-
-https://bitbucket.org/wyatt/dijkstar/src/tip/dijkstar/__init__.py
-
-""",
-    license='BSD/MIT',
+    version='2.0.dev0',
+    description='Dijkstra/A*',
+    long_description=long_description,
+    license='MIT',
     author='Wyatt Lee Baldwin',
     author_email='wyatt.lee.baldwin@gmail.com',
     keywords='Dijkstra A* algorithms',
     url='https://bitbucket.org/wyatt/dijkstar',
-    classifiers=[
+    classifiers=(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python',
-    ],
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
 )
