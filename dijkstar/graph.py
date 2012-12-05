9993647aa19a4ef6e7296c33a79d5b02b4552e3c
Wyatt <self@wyattbaldwin.com>
2012-12-04 18:47:26 -0800

    Simplify graph structure
    The structure is now a simple adjacency matrix::
    
        {node: {neighbor_node: edge, ...}, ...}
    
    Before, the structure was a bit more complicated--the adjacency matrix
    contained pointers to edges in a separate dict instead of the edges
    themselves.
    
    The nodes and edges can be any type of object as long as the nodes are
    hashable, because they are used as dict keys.
    
    Added a simple Graph class to encapsulate this.
    
    single_source_shortest_paths() also now makes less assumptions about
    the types of the nodes and edges a graph contains. In particular, it's
    no longer assumed that edges are lists containing a cost as their first
    item.

---
+++ dijkstar/graph.py
@@ -0,0 +1,42 @@
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+class Graph(dict):
+    """A very simple graph type.
+
+    Its structure looks like this::
+
+        {u: {v: e, ...}, ...}  # Node v is a adjacent to u via edge e
+
+    Nodes and edges can be any type of value. Nodes have to hashable
+    since they're used as dictionary keys.
+
+    """
+
+    def __init__(self, data=None):
+        if data:
+            self.update(data)
+
+    def add_edge(self, u, v, edge=None):
+        self.setdefault(u, {})
+        self[u][v] = edge
+
+    def add_node(self, u, neighbors=None):
+        self.setdefault(u, {})
+        if neighbors:
+            for v, edge in neighbors.items():
+                self[u][v] = edge
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'rb') as loadfile:
+            data = pickle.load(loadfile)
+        graph = cls(data)
+        return graph
+
+    def dump(self, path):
+        with open(path, 'wb') as dumpfile:
+            pickle.dump(self, dumpfile)
