e5b4688c2b4354ff73e1495e158963a866e8bf81
Wyatt <self@wyattbaldwin.com>
2013-09-26 18:59:04 -0700

    Add and improve Graph docstrings

--- dijkstar/graph.py
+++ dijkstar/graph.py
@@ -7,14 +7,15 @@ except ImportError:
 
 
 class Graph(dict):
+
     """A very simple graph type.
 
     Its structure looks like this::
 
         {u: {v: e, ...}, ...}  # Node v is a adjacent to u via edge e
 
-    Nodes and edges can be any type of value. Nodes have to hashable
-    since they're used as dictionary keys.
+    Edges can be of any type. Nodes have to be hashable since they're
+    used as dictionary keys.
 
     """
 
@@ -23,10 +24,19 @@ class Graph(dict):
             self.update(data)
 
     def add_edge(self, u, v, edge=None):
+        """Add an ``edge`` from ``u`` to ``v``."""
         self.setdefault(u, {})
         self[u][v] = edge
 
     def add_node(self, u, neighbors=None):
+        """Add the node ``u``.
+
+        ``neighbors``
+            An optional dict of nodes adjacent to ``u`` and the edges
+            that connect them: {v: edge, ...}. An edge will be added
+            from ``u`` to each ``v`` in ``neighbors``.
+
+        """
         self.setdefault(u, {})
         if neighbors:
             for v, edge in neighbors.items():
@@ -34,10 +44,12 @@ class Graph(dict):
 
     @classmethod
     def load(cls, path):
+        """Load pickled `Graph` from ``path``."""
         with open(path, 'rb') as loadfile:
             return pickle.load(loadfile)
 
     def dump(self, path):
+        """Dump pickled graph to ``path``."""
         with open(path, 'wb') as dumpfile:
             pickle.dump(self, dumpfile)
 
