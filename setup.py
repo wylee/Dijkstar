47054b6dea680fc8d34072ac6d781a0d7c0508c2
Wyatt Lee Baldwin <self@wyattbaldwin.com>
2014-02-01 23:02:04 -0800

    Preparing release 2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README') as fp:
 
 setup(
     name='Dijkstar',
-    version='2.1.dev0',
+    version='2.1',
     description='Dijkstra/A*',
     long_description=long_description,
     license='MIT',
