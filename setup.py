ea30d6fd0883f9a6efda0639a8c125c62e9d1332
Wyatt Baldwin <self@wyattbaldwin.com>
2019-11-17 14:52:37 -0800

    Ensure templates are included in distributions (again)
    Or perhaps this ensures the templates are installed (because I think
    they're already included in distributions).
    
    Amends bea5bf0

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     keywords='Dijkstra A* algorithms',
     url='https://github.com/wylee/Dijkstar',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'runcommands>=1.0a40',
     ],
