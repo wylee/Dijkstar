f1ba2177a426ab8d1edf2a26369b1d2b42f8817a
Seth Junot <xsetech@gmail.com>
2021-03-29 10:45:15 -0700

    Fix deprecated import of MutableMapping from collections
    This DeprecationWarning was raised in the tests under Python 3.8:
    https://github.com/python/cpython/blob/3.8/Lib/collections/__init__.py#L49

--- dijkstar/graph.py
+++ dijkstar/graph.py
@@ -1,6 +1,6 @@
-import collections
 import marshal
 import os
+from collections.abc import MutableMapping
 from copy import copy
 
 try:
@@ -9,7 +9,7 @@ except ImportError:  # pragma: no cover
     import pickle
 
 
-class Graph(collections.MutableMapping):
+class Graph(MutableMapping):
 
     """A very simple graph type.
 
