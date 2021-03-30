bfc5961c4bd0a30934628e0a8382171fb6d49ac0
Wyatt Baldwin <self@wyattbaldwin.com>
2021-03-30 15:03:47 -0700

    Update console script tests for newer version of RunCommands
    In particular, `sys.exit()` needs to be handled in all console script
    tests now.

--- dijkstar/tests/test_console_script.py
+++ dijkstar/tests/test_console_script.py
@@ -1,5 +1,6 @@
 import contextlib
 import io
+import sys
 import unittest
 
 from .. import __version__
@@ -8,35 +9,37 @@ from ..__main__ import main
 
 class TestConsoleScript(unittest.TestCase):
 
+    usage = f'Dijkstar {__version__}\n\nusage: dijkstar'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.exit = sys.exit
+        sys.exit = lambda *args, **kwargs: None
+
+    @classmethod
+    def tearDownClass(cls):
+        sys.exit = cls.exit
+
     def setUp(self):
-        self.stdout = io.StringIO()
-        self.redirected_stdout = contextlib.redirect_stdout(self.stdout)
-
-    @property
-    def stdout_val(self):
-        return self.stdout.getvalue()
-
-    def call_with_redirected_stdout(self, block, args):
-        with self.redirected_stdout:
-            block(args)
+        stdout = io.StringIO()
+        self.redirect_stdout = contextlib.redirect_stdout(stdout)
+        self.get_stdout = lambda: stdout.getvalue()
 
     def test_run_with_no_args(self):
-        self.call_with_redirected_stdout(main.console_script, [])
-        self.assertTrue(self.stdout_val.startswith(f'Dijkstar {__version__}\n\nusage: dijkstar'))
+        with self.redirect_stdout:
+            main.console_script([])
+        self.assertTrue(self.get_stdout().startswith(self.usage))
 
     def test_run_with_help_flag(self):
-        try:
-            self.call_with_redirected_stdout(main.console_script, ['-h'])
-        except SystemExit as exc:
-            self.assertEqual(exc.code, 0)
-        self.assertTrue(self.stdout_val.startswith(f'Dijkstar {__version__}\n\nusage: dijkstar'))
+        with self.redirect_stdout:
+            main.console_script(['-h'])
+        self.assertTrue(self.get_stdout().startswith(self.usage))
 
     def test_run_serve_command(self):
-        self.call_with_redirected_stdout(main.console_script, ['serve'])
+        with self.redirect_stdout:
+            main.console_script(['serve'])
 
     def test_run_serve_with_help_flag(self):
-        try:
-            self.call_with_redirected_stdout(main.console_script, ['serve', '-h'])
-        except SystemExit as exc:
-            self.assertEqual(exc.code, 0)
-        self.assertTrue(self.stdout_val.startswith('usage: dijkstar serve'))
+        with self.redirect_stdout:
+            main.console_script(['serve', '-h'])
+        self.assertTrue(self.get_stdout().startswith('usage: dijkstar serve'))
